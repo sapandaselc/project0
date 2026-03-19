2c8b3d2fcd434f4e81a051ea1877ce4c0677cdbd
dasiot <sapandas1510@gmail.com>
2026-03-19 15:56:26 +0530

    1st

---
+++ script.js
@@ -0,0 +1,20 @@
+const searchBox = document.getElementById("searchBox");
+const cards = document.querySelectorAll(".card");
+
+searchBox.addEventListener("keyup", function () {
+
+    let value = searchBox.value.toLowerCase();
+
+    cards.forEach(function (card) {
+
+        let text = card.innerText.toLowerCase();
+
+        if (text.includes(value)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+
+    });
+
+});
